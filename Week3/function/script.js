707111c124b2f7f9932ac594569472c7643333fa
amass360 <aaronmassoth@gmail.com>
2024-05-26 18:04:54 +0300

    part of function exercise

---
+++ Week3/function/script.js
@@ -0,0 +1,11 @@
+//exercise1
+function family (myAge) {
+    console.log(`My mum is ${myAge * 2}, and my dad is ${myAge * 2 * 1.2}.`)
+}
+
+family(23)
+
+//exercise2
+function family (myAge) {
+    console.log('my mum is ${myAge * 2}')
+}
